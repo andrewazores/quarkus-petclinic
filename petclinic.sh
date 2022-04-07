1ea0201cc7fe12f835f7e60bdb354f0eb54b092c
Andrew Azores <me@andrewazor.es>
2022-04-07 17:43:01 -0400

    setup for demo in OpenShift with Cryostat

--- petclinic.sh
+++ petclinic.sh
@@ -6,3 +6,5 @@ kubectl apply -f src/main/kubernetes/deployment.yml
 kubectl wait --for=condition=available --timeout=60s deployment/quarkus-petclinic
 
 kubectl get services
+oc expose svc quarkus-petclinic
+oc patch -p '{"spec":{"$setElementOrder/ports":[{"port":8080},{"port":9091}],"ports":[{"name":"9091-tcp","port":9091,"protocol":"TCP","targetPort":9091}]}}' svc quarkus-petclinic
